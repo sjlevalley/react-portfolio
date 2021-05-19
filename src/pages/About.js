66cc54dfd661b9058b804f3331a71a9d953e75b2
Steve LeValley <slevalley@gmail.com>
2021-05-18 19:02:46 -0700

    Rough Completed

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,7 @@ import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import MeImage from "../utils/images/MeImage.jpg"
+import "../../src/index.css";
 
 function About(props) {
   return (
@@ -15,32 +16,36 @@ function About(props) {
         </Row>
         <Row>
           <Col size="md-12">
-            <p>
-              <img
-                src={MeImage}
-                alt="A guy holding a fish"
-                className="IDImage float-left p-2"
-              />
-              <strong>Introduction</strong> - Hello! My name is Steve LeValley. I am currently finishing up my certification for
+            <img
+              src={MeImage}
+              alt="A guy holding a fish"
+              className="IDImage float-left p-2"
+            />
+            <div className="container-fluid">
+
+              <p>
+                <strong>Introduction</strong> - Hello! My name is Steve LeValley. I am currently finishing up my certification for
               Full-Stack Web Development through the University of California, San Diego (UCSD)
             </p>
-            <p>
-              <strong>Professional Background</strong> - For the past decade, I have been heavily involved in many aspects of shipboard
+              <p>
+                <strong>Professional Background</strong> - For the past decade, I have been heavily involved in many aspects of shipboard
               engineering related operations in both the Government and Private sectors as well as volunteering in the Navy Reserves.
             </p>
-            <p>
-              <strong>Education</strong> - Along with my Full-Stack Web Development certification, I currently hold a Bachelors
+              <p>
+                <strong>Education</strong> - Along with my Full-Stack Web Development certification, I currently hold a Bachelors
               of Science (B.S.) Degree in Marine Engineering from The California Maritime Academy, which is a university located
               in Vallejo, California and a part of the California State University System. I arrived at the California Maritime
               Academy after attending junior college in both San Diego, California and Santa Barbara, California. I am a San Diego
               native and attended high school at Valhalla High School in El Cajon, CA before beginning my college experience.
             </p>
-            <p>
-              <strong>Personal</strong> - I am a very driven individual and enjoy doing many different activities in my spare time. Those
+              <p>
+                <strong>Personal</strong> - I am a very driven individual and enjoy doing many different activities in my spare time. Those
               activities include, but are not limited to, travelling overseas, taking road trips here in the U.S., participating in athletic endeavors,
               learning about stocks and market economics, barbecuing, working out, taking advantage of our beaches here in San Diego, as well
-              as maintaining and updating my website (one side benefit of taking this web development course).
+              as maintaining and updating my website (one side benefit of taking this web development course). I am looking to continue gaining experience
+              in the web development field and develop my skills as much as I can.
             </p>
+            </div>
 
           </Col>
         </Row>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,31 +1,74 @@
 import React from "react";
+import "../../src/index.css";
+
 
 
 
 function Contact() {
   return (
 
-    <footer className="container-fluid border border-dark" id="contact">
+    <div className="container-fluid border border-dark" id="contact">
+      <div className="row m-2 d-flex contactRow">
+        <h3>My Contact Information</h3>
+      </div>
       <div className="row">
-        <div className="col-3 border border-dark">My Contact Information</div>
-        <div className="col border border-dark">(555) 555-5555</div>
-        <div className="col border border-dark">
-          <h4>
-            <a href="mailto:slevalley@gmail.com" target="_blank" rel="noreferrer">Email Me</a>
-          </h4>
+
+        {/* Phone Contact */}
+        {/* Phone Contact */}
+        <div className="col w-2">
+          <div className="card" >
+            <div className="card-body">
+              <h5 className="card-title">Contact Me by Phone</h5>
+              <p className="card-text">If you have any questions for me or would like to reach out over a phone call,
+              I can be reached at (555) 555-5555.</p>
+            </div>
+          </div>
         </div>
-        <div className="col border border-dark">
-          <h4>
-            <a href="https://github.com/sjlevalley" target="_blank" rel="noreferrer">GitHub</a>
-          </h4>
+
+        {/* Email Contact */}
+        {/* Email Contact */}
+        <div className="col w-2">
+          <div className="card" >
+            <div className="card-body">
+              <h5 className="card-title">Send Me an Email</h5>
+              <p className="card-text">The best way to contact me would be to send me an email at the email address provided in the link below.</p>
+              <h4>
+                <a href="mailto:slevalley@gmail.com" target="_blank" rel="noreferrer">Email Me</a>
+              </h4>
+            </div>
+          </div>
         </div>
-        <div className="col border border-dark">
-          <h4>
-            <a href="https://www.linkedin.com/in/steve-levalley-36550763/" target="_blank" rel="noreferrer">LinkedIn</a>
-          </h4>
+
+        {/* GitHub Link */}
+        {/* GitHub Link */}
+        <div className="col w-2">
+          <div className="card" >
+            <div className="card-body">
+              <h5 className="card-title">View My GitHub Profile</h5>
+              <p className="card-text">Please feel free to view my GitHub profile and repositories at any time, I have provided
+              a link to my profile below.</p>
+              <h4>
+                <a href="https://github.com/sjlevalley" target="_blank" rel="noreferrer">GitHub</a>
+              </h4>
+            </div>
+          </div>
+        </div>
+
+        {/* LinkedIn Link */}
+        {/* LinkedIn Link */}
+        <div className="col w-2">
+          <div className="card" >
+            <div className="card-body">
+              <h5 className="card-title">View My LinkedIn Profile</h5>
+              <p className="card-text">You can also view my LinkedIn profile at the link provided below.</p>
+              <h4>
+                <a href="https://www.linkedin.com/in/steve-levalley-36550763/" target="_blank" rel="noreferrer">LinkedIn</a>
+              </h4>
+            </div>
+          </div>
         </div>
       </div>
-    </footer>
+    </div >
   );
 }
 
